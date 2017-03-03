9758d60f499948894d60675f9c50eaebbc38ee30
michaelj <michaelj@208.91.197.27>
2017-03-03 12:58:20 -0500

    Commit

---
+++ spring-thymeleaf/src/test/java/guru/springframework/controllers/IndexControllerTestIT.java
@@ -0,0 +1,11 @@
+package guru.springframework.controllers;
+
+import org.junit.Test;
+
+public class IndexControllerTestIT {
+
+	@Test
+	public void test(){
+		System.out.println("In integration test");
+	}
+}
